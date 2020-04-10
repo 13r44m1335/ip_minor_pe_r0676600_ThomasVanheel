e6008597f8a8e6e0b5185541f6c8122471e44c42
13r44m1335 <thomas_vanheel@outlook.com>
2020-04-10 13:32:16 +0200

    start

---
+++ task-r0676600/src/main/java/com/example/taskr0676600/TaskR0676600Application.java
@@ -0,0 +1,13 @@
+package com.example.taskr0676600;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskR0676600Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskR0676600Application.class, args);
+	}
+
+}
---
+++ task-r0676600/src/test/java/com/example/taskr0676600/TaskR0676600ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taskr0676600;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskR0676600ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
